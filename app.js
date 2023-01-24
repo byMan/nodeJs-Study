78dadbf4ab02d199ccc341561ee009b476ee94c6
naya279 <naya279@naya279ui-MacBookPro.local>
2023-01-24 16:19:34 +0900

    login modified

--- app.js
+++ app.js
@@ -11,5 +11,6 @@ const home = require("./src/routes/home");  //index.js 파일을 읽어온다.
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use("/", home);   // use -> 미들 웨어를 등록해주는 메서드
+app.use(express.static(`${__dirname}/src/public`));
 
 module.exports = app;
